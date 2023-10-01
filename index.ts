77dd6448992d70fde0485f0628f283d9656f3f81
glxymesh <abhishekmourya6774@gmail.com>
2023-10-01 14:22:00 +0530

    saveuser function creation and testing

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
-import { factorial, fibonacci } from "./utils";
+import { User, factorial, fibonacci, saveUser } from "./utils";
 
 (function main() {
+    const user: User = {
+        name: "John Doe",
+        age: 25,
+        contact: "1234567890",
+        email: "jhon@mail.com",
+        verfied: false,
+    }
+
+
     console.log("Factorial:", factorial(25));
-    console.log("Fibonacci:", fibonacci(50));
+    console.log("Fibonacci:", fibonacci(10));
+
+    saveUser(user);
 })();
--- utils.ts
+++ utils.ts
@@ -1,3 +1,13 @@
+import crypto from "crypto";
+
+export interface User {
+    name: string;
+    age: number;
+    contact: string;
+    email: string;
+    verfied: boolean;
+}
+
 export function factorial(n: number) {
     if (n === 0) {
         return 1;
@@ -5,10 +15,20 @@ export function factorial(n: number) {
     return n * factorial(n - 1);
 }
 
-
 export function fibonacci(n: number) {
     if (n === 0 || n === 1) {
         return 1;
     }
     return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
+export async function saveUser(user: User) {
+    console.log("Saving user...");
+    const savedUser = await new Promise<User & { uid: string }>((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ ...user, uid: crypto.randomBytes(16).toString("hex") });
+        }, 2000);
+    })
+    console.log(`User ${user.name} saved with ${savedUser.uid} Saved!`)
 }
